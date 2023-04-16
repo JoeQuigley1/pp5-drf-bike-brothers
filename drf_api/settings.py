7e6385d61cd4ef3432cd3395bd1cbb373d7f90de
JoeQuigley1 <joeyjtq@hotmail.com>
2023-04-16 19:21:22 +0000

    Turn off debug

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -64,7 +64,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
-DEBUG = True
+# DEBUG = True
 DEBUG = 'DEV' in os.environ
 
 ALLOWED_HOSTS = [
