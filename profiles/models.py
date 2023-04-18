da7dd2c55f502c69b713b2a38c50a05e9d91a9f2
JoeQuigley1 <joeyjtq@hotmail.com>
2023-04-18 18:19:47 +0000

    Make changes to the default image to try and fix 404 error on FE

--- profiles/models.py
+++ profiles/models.py
@@ -15,7 +15,7 @@ class Profile(models.Model):
     name = models.CharField(max_length=255, blank=True)
     content = models.TextField(blank=True)
     image = models.ImageField(
-        upload_to='images/', default='../default_profile_ua3v6g'
+        upload_to='images/', default='media/images/default_profile_ua3v6g'
     )
 
     class Meta:
